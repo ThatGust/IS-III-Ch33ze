ad0a4f36011b35f1f333164a61b303fded354b72
ThatGust <73896944+ThatGust@users.noreply.github.com>
2023-06-13 11:20:29 -0500

    Delete src directory

--- src/project_name/lib/healthchecks.py
+++
@@ -1,5 +0,0 @@
-from django.conf import settings
-
-
-def git_version():
-    return getattr(settings, 'GIT_VERSION', 'Unknown')
--- src/tests/test_wsgi.py
+++
@@ -1,3 +0,0 @@
-def test_wsgi():
-    import {{ project_name }}.wsgi.development
-    import {{ project_name }}.wsgi.docker
