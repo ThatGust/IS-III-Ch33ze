8ee0b294842d6d78e09f37d8a580502b71c18ebd
Diego-Aquino1 <daquino@unsa.edu.pe>
2023-07-06 01:51:34 -0500

    migrate flask 1

--- manage.py
+++ manage.py
@@ -1,22 +1,79 @@
-#!/usr/bin/env python
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
+from flask import Flask, request, jsonify, render_template, redirect, flash
 
-import os
-import sys
+import json
+import requests
 
-def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+from flask_cors import CORS, cross_origin
 
-if __name__ == '__main__':
-    main()
+from Backend.blueprints.task_blueprint import task_blueprint
+from Backend.models.task_model import TaskModel
+
+host = 'http://localhost:5000'
+
+model = TaskModel()
+
+app = Flask(__name__,template_folder='Frontend',static_folder='Frontend/static')
+# para que utilice vue compilado ( npm run build ). En la carpeta dist, esta lo compilado de vue
+# app = Flask(__name__, template_folder= './Frontend')
+
+app.register_blueprint(task_blueprint)
+
+cors = CORS(app)
+app.config['SECRET_KEY'] = 'keyy'
+
+def authenticate_user(username, password):
+
+    user = model.get_user_username(username)
+    if user and user['password'] == password:
+        return user
+    return None
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('home/index.html')
+
+@app.route('/profesores')
+def teacher():
+    return render_template('home/teachers.html')
+
+@app.route('/correos')
+def mail():
+    return render_template('home/mails.html')
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        user = authenticate_user(username, password)
+        if user:
+            return redirect('/dashboard')
+        else:
+            flash('Error, intentelo de nuevo', 'error')
+
+    return render_template('accounts/login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+
+        username = request.form['username']
+        password = request.form['password']
+
+        data = {
+            'username' : username,
+            'password' : password
+        }
+
+        headers = {'Content-Type': 'application/json'}
+        response = requests.post(host + '/user/add_user', json=data, headers=headers)
+
+        return redirect('/')
+    
+    return render_template('accounts/register.html')
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
