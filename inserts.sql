fe4e86d7e3a18d3d29b386736b6376cb48c20ff4
Fabrizio Mattos <fmattos@unsa.edu.pe>
2023-08-03 00:48:47 -0500

    Addedcourses to database

---
+++ inserts.sql
@@ -0,0 +1,208 @@
+INSERT INTO users(username,password)VALUES("admin","admin");
+INSERT INTO administrador(id)VALUES(1);
+INSERT INTO users(username,password) VALUES("prof1","123456");
+INSERT INTO user_info(iduser,nombre,apellidoP,apellidoM,dni,correo,celular,ciudad)
+VALUES(2,"Juan","Mendoza","Choque","12345678","jmendoza@gmail.com","954678126","Arequipa");
+INSERT INTO profesor(id,datePos,curso,estado)
+VALUES(2,"02/08/2023","Algoritmos y Estructuras de Datos","Pendiente");
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Razonamiento Lógico Matemático",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Metodología del Trabajo Intelectual Universitario",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Arte Computacional",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Ingles Técnico Profesional",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Fundamentos de Computación",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Estructuras Discretas",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Liderazgo y Oratoria",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Estructuras Discretas II",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Ciencia de la Computación I",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Calculo en una Variable",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Ingles Técnico Profesional II",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Comunicación Integral",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Realidad Nacional",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Arquitectura de Computadores",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Ciencia de la Computación II",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Desarrollo Basado en Plataformas",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Trabajo interdisciplinar I",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Calculo en Varias Variables",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Ingles Ténico Profesional III",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Ciudadanía e Interculturalidad",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Algoritmos y Estructuras de Datos",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Teoría de la Computación",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Base de Datos I",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Algebra Lineal Numérica",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Estadística y Probabilidades",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Ecología y Conservación Ambiental",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Base de Datos II",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Análisis y Diseño de Algoritmos",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Ingeniería de Software I",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Compiladores",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Análisis Exploratorio de Datos Espaciales",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Ecuaciones Diferenciales",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Programación Competitiva",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Estructura de Datos Avanzados",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Sistemas Operativos",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Trabajo Interdisciplinar II",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Matemática Aplicada a la Computación",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Investigación en Ciencia de la Computación",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Desarrollo de Software Empresarial",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Redes y Comunicación",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Ingeniería de Software III",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Computación Gráfica",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Inteligencia Artificial",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Interacción Humano Computador",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Proyecto de Final de Carrera I",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Computación Paralela y Distribuida",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Seguridad en Computación",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Formación de Empresas de Base Tecnológica I",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Relaciones Humanas",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Fisica Computacional",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Tópicos en Ingeniería de Software",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Proyecto de Final de Carrera II",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Big Data",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Ética General y Profesional",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Formación de Empresas de Base Tecnológica",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Ingles Técnico Profesional IV",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Tópicos en Ciencia de Datos",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Tópicos en Inteligencia Artificial",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Tópicos en Computación Gráfica",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Bioinformática",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Cloud Computing",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Proyecto de Final de Carrera III",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Trabajo Interdisciplinar III",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Internet de las Cosas",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Robótica",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Tópicos en Ciberseguridad",21);
+
+INSERT INTO curso(nombre,n_alumnos)
+VALUES("Practicas Preprofesionales",21);
